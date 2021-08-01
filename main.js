c081a963518454b1e209d45cd549d7bc54eae02a
NaethanLeo <75014295+NaethanLeo@users.noreply.github.com>
2021-08-01 22:26:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+prediction1 = "";
+prediction2 = "";
+
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 200
+});
+
+camera = document.getElementById("camera");
+Webcam.attach("#camera");
+
+function snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = "<img id='image' src='"+data_uri+"'>"
+    });
+}
+console.log("ml5 version =", ml5.version);
+
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/kgek0glbz/model.json", modelLoaded);
+
+function modelLoaded(){
+    console.log("model Loaded")
+}
